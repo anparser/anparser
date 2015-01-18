284c315853314b8563500cf6823274447e1815a3
Chapin Bryce <4n6kid@users.noreply.github.com>
2015-01-18 18:41:08 -0500

    Updated to allow for unittest data to reference

--- anparser/test.py
+++ anparser/test.py
@@ -25,8 +25,6 @@ import unittest
 
 from plugins import xml_plugins
 from plugins import sqlite_plugins
-from plugins import other_plugins
-from writers import csv_writer
 
 
 class TestAnParser(unittest.TestCase):
@@ -35,7 +33,7 @@ class TestAnParser(unittest.TestCase):
         pass
 
     def test_xmlFiles(self):
-        self.xml_return = xml_plugins.parse_xml_file_notree('tests/test.xml')
+        self.xml_return = xml_plugins.parse_xml_file_notree('anparser/tests/test.xml')
 
         self.expected_data = [{'name': 'ds__downloads_json__',
                           'text_entry': '[{"minVersion":0,"filename":"libAppDataSearchExt_armeabi_v7a.so",'
@@ -46,21 +44,21 @@ class TestAnParser(unittest.TestCase):
         self.assertEqual(self.xml_return, self.expected_data)
 
     def test_sqliteGetTableNames(self):
-        self.sqlite_table_names_return = sqlite_plugins.get_sqlite_table_names('tests/test.sqlite')
+        self.sqlite_table_names_return = sqlite_plugins.get_sqlite_table_names('anparser/tests/test.sqlite')
 
         self.expected_data = ['test1', 'test2']
 
         self.assertEqual(self.sqlite_table_names_return, self.expected_data)
 
     def test_sqliteGetTableRows(self):
-        self.sqlite_table_rows_return = sqlite_plugins.read_sqlite_table('tests/test.sqlite', 'test1')
+        self.sqlite_table_rows_return = sqlite_plugins.read_sqlite_table('anparser/tests/test.sqlite', 'test1')
 
         self.expected_data = [(u'1', u'Dan', u'33'), (u'2', u'Jon', u'23')]
 
         self.assertEqual(self.sqlite_table_rows_return, self.expected_data)
 
     def test_sqliteGetTablesRows(self):
-        self.sqlite_tables_rows_return = sqlite_plugins.read_sqlite_tables('tests/test.sqlite')
+        self.sqlite_tables_rows_return = sqlite_plugins.read_sqlite_tables('anparser/tests/test.sqlite')
 
         self.expected_data = {u'test1': [(u'1', u'Dan', u'33'), (u'2', u'Jon', u'23')], u'test2': [(u'1',
                                                                                                     u'New York City',
