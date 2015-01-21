9f0330f2eaee773cfb8edbc0102cc3caab954fbe
prmiller91 <prmiller91@gmail.com>
2015-01-20 19:44:16 -0500

    Potential CSV UTF-8 Fix.

--- anparser/writers/csv_writer.py
+++ anparser/writers/csv_writer.py
@@ -22,7 +22,37 @@ __date__ = '20150102'
 __version__ = '0.00'
 
 import csv
+import cStringIO
+import codecs
 import logging
+
+class DictUnicodeWriter(object):
+
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, D):
+        self.writer.writerow({k:v.encode("utf-8") if isinstance(v, unicode) else v for k,v in D.items()})
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for D in rows:
+            self.writerow(D)
+
+    def writeheader(self):
+        self.writer.writeheader()
 
 
 def csv_writer(data, file_name):
@@ -36,7 +66,7 @@ def csv_writer(data, file_name):
 
     fout = open(file_name, mode='wb')
     try:
-        writer = csv.DictWriter(fout, data[0].keys(), delimiter='|')
+        writer = DictUnicodeWriter(fout, data[0].keys(), delimiter='|')
         writer.writeheader()
         writer.writerows(data)
     except (IndexError, TypeError) as exception:
@@ -47,3 +77,4 @@ def csv_writer(data, file_name):
     fout.close()
 
     return True
+
