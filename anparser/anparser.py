e4d07a98aa173fdb99fa6493bbb2075695e85dc6
Chapin Bryce <4n6kid@users.noreply.github.com>
2015-01-28 21:16:24 -0500

    Initial dashboard with account overview table

--- anparser/anparser.py
+++ anparser/anparser.py
@@ -621,33 +621,17 @@ if __name__ == "__main__":
     logging.info(msg)
     print(msg)
 
-    # Write Overview
-    htmlWriter = writers.html_writer.BootstrapHTML(args.destination + '//Overview.html')
-    htmlWriter.write(htmlWriter.header)
-    htmlWriter.setup_account_information()
-    account_set = set()
 
-    for item in vending_localapp_list:
-        if item['Account']:
-            data = ('Android Account', item['Account'])
-            account_set.add(data)
-    htmlWriter.add_account_information(account_set)
-    account_set = set()
+    # Build data_dict for Overview Parser
+    data_dict = dict()
+    data_dict['Android Vending LocalApp List'] = vending_localapp_list
+    data_dict['Google Docs Account Data'] = google_docs_account_data
+    data_dict['Google Plus Accounts'] = google_plus_accounts
 
-    for item in google_docs_account_data:
-            if item['Account Name']:
-                data = ('Google Docs Account', item['Account Name'])
-                account_set.add(data)
-    htmlWriter.add_account_information(account_set)
-    account_set = set()
-
-    for item in google_plus_accounts:
-            if item['Account Name']:
-                data = ('Google Plus Account', item['Account Name'])
-                account_set.add(data)
-    htmlWriter.add_account_information(account_set)
-    htmlWriter.close_outfile()
-
+    dash = writers.html_writer.OverviewParser(data_dict, args.destination)
+    dash.create_dashboard()
+    dash.add_section_accounts()
+    dash.close_dashboard()
 
 
     # Write Contact Data
