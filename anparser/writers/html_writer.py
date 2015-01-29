972f2e58e0dfb54bba26bd2e4cec7dbcda3fef22
Chapin Bryce <4n6kid@users.noreply.github.com>
2015-01-28 19:57:58 -0500

    Built basic HTMLDashboardWriter class

--- anparser/writers/html_writer.py
+++ anparser/writers/html_writer.py
@@ -21,82 +21,88 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os
 
-class BootstrapHTML():
+class OverviewParser():
+    """
+    Used to parse the input data and extract the useful information to write with HTMLDashboardWriter()
+    """
 
-    def __init__(self, outfile=None):
-        self.outfile = outfile
-        self.outfile = self.open_outfile()
-        self.title = None
-        self.header = """
+    def __init__(self):
+        pass
+
+
+class HTMLDashboardWriter():
+    """
+    Writes standard data to an HTML file
+    """
+    def __init__(self):
+        self.header = '''
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="html_support/bootstrap.min.css">
-<script src="html_support/jquery.min.js"></script>
-<script src="html_support/bootstrap.min.js"></script>
-</head>
+  <head>
+    <meta charset="utf-8">
+    <!-- Bootstrap core CSS -->
+    <link href="html_support/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="html_support/dashboard.css" rel="stylesheet">
+  </head>
+'''
+        self.title = ''
+        self.navbar = '''
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+      aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">AnParser Dashboard</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Dashboard</a></li>
+      </ul>
+      <form class="navbar-form navbar-right">
+        <input type="text" class="form-control" placeholder="Search...">
+      </form>
+    </div>
+  </div>
+</nav>
+        '''
+        self.dashboard_first_section = '''<div class="container-fluid"> <h1 class="page-header">Dashboard</h1>'''
+        self.dashboard_section_title = ''
+        self.dashboard_section = '<h2 class="sub-header">' + self.dashboard_section2_title + '</h2>'
+        self.dashboard_section_table_headers = []
+        self.dashboard_section_table_data = []
+
+    def _build_table_headers(self):
         """
+        Private class that builds out the needed components for a table header
+        :return: string html of table header
+        """
+        table_writer = '<div class="table-responsive"><table class="table table-striped"><thead><tr>'
+        for table_name in self.dashboard_section_table_headers:
+            table_writer += '<th>' + table_name + '</th>'
 
-        self.jumbo = """<div class="container">
-    <div class="jumbotron">
-        <h1>AnParser Overview</h1>
+        table_writer += '</tr></thead>'
 
-        <small>Overview of artifacts from AnParser Results of [Insert Path here]</small>
-    </div>"""
+        return table_writer
 
-        # Write Jumbo Header
-        self.write(self.jumbo)
+    def _build_table_data(self):
+        """
+        Private class that builds out the table
+        :return: string html of table data
+        """
+        table_writer = '<tbody>'
+        for row in self.dashboard_section_table_data:
+            table_writer += '<tr>'
+            for cell in row:
+                table_writer += '<td>' + cell + '</rd>'
+            table_writer += '</tr>'
 
-    def open_outfile(self):
-        return open(self.outfile, 'w')
+        table_writer += '</tbody></table></div>'
 
-    def close_outfile(self):
-
-        # Write Footer
-        self.write("""
-                    </div>
-                    </body>
-                    </head>
-                    </html>
-                    """)
-
-        try:
-            self.outfile.close()
-        except IOError:
-            IOError('Unable to write to output file')
-
-    def write(self, information):
-        self.outfile.write(information)
-
-
-    def setup_account_information(self):
-        setup = """
-            <h2>Account Information:</h2>
-            <div class="row">"""
-        self.write(setup)
-
-    def add_account_information(self, information):
-
-        for info in information:
-            try:
-                account_type = info[0]
-                account_value = info[1]
-                data = """
-                        <div class="col-sm-4">
-                            <p>""" + account_type + """</p>
-                        </div>
-                        <div class="col-sm-8">
-                            <p>""" + account_value + """</p>
-                        </div>
-                        """
-
-            except:
-                data = ''
-                pass
-
-            self.write(data)
-
+        return table_writer
