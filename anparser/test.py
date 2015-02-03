14d500f20fb0254d67049568bf3c6038a63df055
prmiller91 <prmiller91@gmail.com>
2015-02-02 21:48:13 -0500

    Updated test.py to reflect changes to sqlite ingests.

--- anparser/test.py
+++ anparser/test.py
@@ -25,9 +25,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 
-from plugins import xml_plugins
-from plugins import sqlite_plugins
-
+from ingest import xml_processor
+from ingest import sqlite_processor
+from pandas.util.testing import assert_frame_equal
+import pandas as pd
 
 class TestAnParser(unittest.TestCase):
 
@@ -35,7 +36,7 @@ class TestAnParser(unittest.TestCase):
         pass
 
     def test_xmlFiles(self):
-        self.xml_return = xml_plugins.parse_xml_file_notree('anparser/tests/test.xml')
+        self.xml_return = xml_processor.parse_xml_file_notree('anparser/tests/test.xml')
 
         self.expected_data = [{'name': 'ds__downloads_json__',
                           'text_entry': '[{"minVersion":0,"filename":"libAppDataSearchExt_armeabi_v7a.so",'
@@ -46,28 +47,31 @@ class TestAnParser(unittest.TestCase):
         self.assertEqual(self.xml_return, self.expected_data)
 
     def test_sqliteGetTableNames(self):
-        self.sqlite_table_names_return = sqlite_plugins.get_sqlite_table_names('anparser/tests/test.sqlite')
+        self.sqlite_table_names_return = sqlite_processor.get_sqlite_table_names('anparser/tests/test.sqlite')
 
         self.expected_data = [u'test1', u'test2']
 
         self.assertEqual(self.sqlite_table_names_return, self.expected_data)
 
     def test_sqliteGetTableRows(self):
-        self.sqlite_table_rows_return = sqlite_plugins.read_sqlite_table('anparser/tests/test.sqlite', 'test1')
+        self.sqlite_table_rows_return = sqlite_processor.read_sqlite_table('anparser/tests/test.sqlite', 'test1')
 
-        self.expected_data = [{'age': u'33', 'id':u'1', 'name':u'Dan'}, {'age':u'23', 'id':u'2', 'name':u'Jon'}]
+        self.expected_data = pd.DataFrame([{'age': u'33', 'id':u'1', 'name':u'Dan'},
+                                          {'age':u'23', 'id':u'2', 'name':u'Jon'}])
 
-        self.assertEqual(self.sqlite_table_rows_return, self.expected_data)
+        assert_frame_equal(self.sqlite_table_rows_return.sort(axis=1), self.expected_data.sort(axis=1))
+
 
     def test_sqliteGetTablesRows(self):
-        self.sqlite_tables_rows_return = sqlite_plugins.read_sqlite_tables('anparser/tests/test.sqlite')
+        self.sqlite_tables_rows_return = sqlite_processor.read_sqlite_tables('anparser/tests/test.sqlite')
 
-        self.expected_data = {u'test1': [{'age': u'33', 'id':u'1', 'name':u'Dan'},
-                                         {'age':u'23', 'id':u'2', 'name':u'Jon'}],
-                              u'test2': [{'city':u'New York City', 'state':u'NY', 'id':u'1'},
-                                          {'city':u'Portland', 'state':u'OR', 'id':u'2'}]}
+        self.expected_data = {u'test1': pd.DataFrame([{'age': u'33', 'id':u'1', 'name':u'Dan'},
+                                         {'age':u'23', 'id':u'2', 'name':u'Jon'}]),
+                              u'test2': pd.DataFrame([{'city':u'New York City', 'state':u'NY', 'id':u'1'},
+                                          {'city':u'Portland', 'state':u'OR', 'id':u'2'}])}
 
-        self.assertEqual(self.sqlite_tables_rows_return, self.expected_data)
+        for (f1, f2) in zip(self.sqlite_tables_rows_return.values(), self.expected_data.values()):
+            assert_frame_equal(f1.sort(axis=1), f2.sort(axis=1))
 
 if __name__ == '__main__':
     # TODO: Add sqlite views test
